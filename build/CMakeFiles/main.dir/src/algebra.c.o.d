3f972340681382019f83610dcb583a985d3c0b6e
SuperbRa1n <tanninrachel@yinlin.wiki>
2024-03-28 19:52:27 +0800

    题目要求基本实现

--- build/CMakeFiles/main.dir/src/algebra.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/src/algebra.c.o: \
- /Users/zzy/Desktop/大三下/XLab/hw/src/algebra.c
--- src/main.c
+++ src/main.c
@@ -3,6 +3,42 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    while (1)
+    {
+        int frame;
+        scanf("%d", &frame);
+        if (frame == -1)
+        {
+            break;
+        }
+        int row, col;
+        scanf("%d %d\n", &row, &col);
+        Matrix a = create_matrix(row, col);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                scanf("%lf", &a.data[i][j]);
+            }
+        }
+        scanf("%d %d\n", &row, &col);
+        Matrix b = create_matrix(row, col);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                scanf("%lf", &b.data[i][j]);
+            }
+        }
+        print_matrix(add_matrix(a, b));
+        print_matrix(sub_matrix(a, b));
+        print_matrix(mul_matrix(a, b));
+        print_matrix(scale_matrix(a, 2));
+        print_matrix(transpose_matrix(a));
+        printf("%.2f\n", det_matrix(a));
+        print_matrix(inv_matrix(a));
+        printf("%d\n", rank_matrix(b));
+        printf("%.2f\n", trace_matrix(a));
+    }
     return 0;
 }
