ad6087c582573b28cecafb4d9770e1ca9722a5a2
SuperbRa1n <tanninrachel@yinlin.wiki>
2024-03-29 23:08:45 +0800

    v1.0.2 微调注释

--- inc/algebra.h
+++ inc/algebra.h
@@ -21,7 +21,7 @@ Matrix create_matrix(int rows, int cols);
  * @brief 矩阵加法
  * @param a 矩阵a
  * @param b 矩阵b
- * @return a + b, 如果a和b的行数或列数不相等，会给出错误提示并返回一个空矩阵
+ * @return a + b, 如果a和b的行数或列数不相等，会给出错误提示"Error: Matrix a and b must have the same rows and cols.\n"并返回一个空矩阵
 */
 Matrix add_matrix(Matrix a, Matrix b);
 
@@ -29,7 +29,7 @@ Matrix add_matrix(Matrix a, Matrix b);
  * @brief 矩阵减法
  * @param a 矩阵a
  * @param b 矩阵b
- * @return a - b，如果a和b的行数或列数不相等，会给出错误提示并返回一个空矩阵
+ * @return a - b，如果a和b的行数或列数不相等，会给出错误提示"Error: Matrix a and b must have the same rows and cols.\n"并返回一个空矩阵
 */
 Matrix sub_matrix(Matrix a, Matrix b);
 
@@ -37,7 +37,7 @@ Matrix sub_matrix(Matrix a, Matrix b);
  * @brief 矩阵乘法
  * @param a 矩阵a
  * @param b 矩阵b
- * @return a * b，如果a的列数不等于b的行数，会给出错误提示并返回一个空矩阵
+ * @return a * b，如果a的列数不等于b的行数，会给出错误提示"Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n"并返回一个空矩阵
 */
 Matrix mul_matrix(Matrix a, Matrix b);
 
@@ -59,14 +59,14 @@ Matrix transpose_matrix(Matrix a);
 /**
  * @brief 矩阵的行列式
  * @param a 矩阵a
- * @return a的行列式, 如果a不是方阵，会给出错误提示并返回0
+ * @return a的行列式, 如果a不是方阵，会给出错误提示"Error: The matrix must be a square matrix.\n"并返回0
 */
 double det_matrix(Matrix a);
 
 /**
  * @brief 矩阵的逆
  * @param a 矩阵a
- * @return a的逆矩阵, 如果a不是方阵或a的行列式为0，会给出错误提示并返回一个空矩阵
+ * @return a的逆矩阵, 如果a不是方阵或a的行列式为0，会给出错误提示"Error: The matrix must be a square matrix.\n"并返回一个空矩阵，如果a的逆矩阵不存在，会给出错误提示"Error: The matrix is singular.\n"并返回一个空矩阵。
 */
 Matrix inv_matrix(Matrix a);
 
@@ -80,7 +80,7 @@ int rank_matrix(Matrix a);
 /**
  * @brief 矩阵的迹
  * @param a 矩阵a
- * @return a的迹，如果a不是方阵，会给出错误提示并返回0
+ * @return a的迹，如果a不是方阵，会给出错误提示"Error: The matrix must be a square matrix.\n"并返回0
 */
 double trace_matrix(Matrix a);
 
