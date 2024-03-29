9b1d1e41de80470bc184d9c175f51b35e4681d3b
SuperbRa1n <tanninrachel@yinlin.wiki>
2024-03-29 23:02:40 +0800

    v1.0.0发行版

--- src/main.c
+++ src/main.c
@@ -5,40 +5,191 @@ int main()
 {
     while (1)
     {
-        int frame;
-        scanf("%d", &frame);
-        if (frame == -1)
+        char op;
+        scanf("%c", &op);
+        switch(op) 
         {
-            break;
-        }
-        int row, col;
-        scanf("%d %d\n", &row, &col);
-        Matrix a = create_matrix(row, col);
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
+            case '+':
             {
-                scanf("%lf", &a.data[i][j]);
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                scanf("%d %d", &rows, &cols);
+                Matrix b = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &b.data[i][j]);
+                    }
+                }
+                Matrix c = add_matrix(a, b);
+                print_matrix(c);
+                break;
+            }
+            case '-':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                    scanf("\n");
+                }
+                scanf("%d %d", &rows, &cols);
+                Matrix b = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &b.data[i][j]);
+                    }
+                }
+                Matrix c = sub_matrix(a, b);
+                print_matrix(c);
+                break;
+            }
+            case '*':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                scanf("%d %d\n", &rows, &cols);
+                Matrix b = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &b.data[i][j]);
+                    }
+                }
+                Matrix c = mul_matrix(a, b);
+                print_matrix(c);
+                break;
+            }
+            case '.':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                Matrix b = scale_matrix(a, 2.0);
+                print_matrix(b);
+                break;
+            }
+            case 't':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                Matrix b = transpose_matrix(a);
+                print_matrix(b);
+                break;
+            }
+            case 'd':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                double det = det_matrix(a);
+                printf("%.2f\n", det);
+                break;
+            }
+            case 'i':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                Matrix b = inv_matrix(a);
+                print_matrix(b);
+                break;
+            }
+            case 'r':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                int rank = rank_matrix(a);
+                printf("%d\n", rank);
+                break;
+            }
+            case 'j':
+            {
+                int rows, cols;
+                scanf("%d %d", &rows, &cols);
+                Matrix a = create_matrix(rows, cols);
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        scanf("%lf", &a.data[i][j]);
+                    }
+                }
+                double trace = trace_matrix(a);
+                printf("%.2f\n", trace);
+                break;
+            }
+            case 'q':
+            {
+                return 0;
+            }
+            default:
+            {
+                continue;
             }
         }
-        scanf("%d %d\n", &row, &col);
-        Matrix b = create_matrix(row, col);
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                scanf("%lf", &b.data[i][j]);
-            }
-        }
-        print_matrix(add_matrix(a, b));
-        print_matrix(sub_matrix(a, b));
-        print_matrix(mul_matrix(a, b));
-        print_matrix(scale_matrix(a, 2));
-        print_matrix(transpose_matrix(a));
-        printf("%.2f\n", det_matrix(a));
-        print_matrix(inv_matrix(a));
-        printf("%d\n", rank_matrix(b));
-        printf("%.2f\n", trace_matrix(a));
     }
     return 0;
 }
